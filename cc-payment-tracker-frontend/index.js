2b9bc4d6a060ded3c43bec15ebf9249db558a2c2
ashok997 <ashok997@gmail.com>
2020-01-22 09:15:43 -0600

    added the basic form to create new card

--- cc-payment-tracker-frontend/index.js
+++ cc-payment-tracker-frontend/index.js
@@ -1,1 +1,5 @@
-console.log("linked");
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000/api/v1/credit_cards")
+        .then(res => res.json())
+        .then(data => console.log(data));
+})
