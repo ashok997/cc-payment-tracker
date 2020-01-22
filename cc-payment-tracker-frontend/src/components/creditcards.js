c308cf3fbcda9397c89f95aead4de8dc40e37093
ashok997 <ashok997@gmail.com>
2020-01-22 10:56:09 -0600

    added fetchandloadCreditCards

--- cc-payment-tracker-frontend/src/components/creditcards.js
+++ cc-payment-tracker-frontend/src/components/creditcards.js
@@ -2,13 +2,21 @@ class CreditCards {
     constructor() {
         this.creditcards = []
         this.adapter = new CreditCardsAdapter()
-        //this.bindEventListeners()
         this.fetchAndLoadCreditCards()
     }
 
     fetchAndLoadCreditCards() {
         this.adapter.getCreditCards().then(data => {
-            console.log(data)
+            data.forEach(card => this.creditcards.push(card))
         })
+            .then(() => {
+                this.render()
+            })
+    }
+
+    render() {
+        const creditCardsContainer = document.getElementById("credit-cards-container")
+        creditCardsContainer.innerHTML = 'my cards here'
+        console.log("cards availabe", this.creditcards)
     }
 }
