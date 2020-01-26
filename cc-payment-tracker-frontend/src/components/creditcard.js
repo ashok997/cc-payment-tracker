b43769d7428f2e49b42deed740684fc43e4b9fc5
ashok997 <ashok997@gmail.com>
2020-01-26 14:33:39 -0600

    added form to record transaction

--- cc-payment-tracker-frontend/src/components/creditcard.js
+++ cc-payment-tracker-frontend/src/components/creditcard.js
@@ -9,7 +9,7 @@ class CreditCard {
     }
 
     renderCard() {
-        return `<ul>
+        return `<ul-data-cardId=${this.id}>
                     <li>${this.card_name}</li>
                     <li>${this.last_four}</li>
                     <li>${this.exp_date}</li>
@@ -32,7 +32,20 @@ class CreditCard {
 
 
     renderAll() {
-        return ` ${this.renderCard()} ${this.renderCardTransactions()} `
+        return ` ${this.renderCard()} ${this.renderCardTransactions()}
+          
+            <form id="new-transaction-form">
+            <div class="input-field">
+                <label for="amount">Amount </label>    
+                <input type="number" step="0.01" name="amount" id="amount">
+            </div>
+            <div class="input-field">
+                <label for="date">Date </label>    
+                <input type="date" name="date" id="date">
+            </div>
+            <input type="submit" value="Record Transaction">
+        </form>`
+        //<button onclick="newTransaction()">Record Transaction</button>`
     }
 
 }
